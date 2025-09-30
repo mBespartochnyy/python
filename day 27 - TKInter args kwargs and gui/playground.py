98f0857dc61928f7ca105b216c2889d19e8c30a3
Mikhail Bespartochnyy <mbespartochnyy@gmail.com>
2025-09-30 19:38:48 -0400

    Add example code that would handle scenarios when user hasn't provided an argument for one of the named parameters.

--- day 27 - TKInter args kwargs and gui/playground.py
+++ day 27 - TKInter args kwargs and gui/playground.py
@@ -1,7 +1,19 @@
-def add(*args):
+def add(*nums_arr):
 	result = 0
-	for n in args:
+	for n in nums_arr:
 		result += n
 	print(result)
 
 add(1,3,5,7, -6)
+
+class Car:
+	def __init__(self, **kw):
+		self.make = kw.get('make')
+		self.model = kw.get('model')
+
+my_car = Car(make='Nissan')
+print(my_car.model)
+# output: None
+
+print(my_car.make)
+# output: Nissan
